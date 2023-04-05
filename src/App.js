88664c13a3360e4530b24ac31b81bf681d604c74
AJ Murray <aj.murr4y@gmail.com>
2023-04-05 12:22:00 -0400

    added in a form and a header, no changes made from example for in reactstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
+import { Form, Col, Label, Input, FormGroup, Button, Row } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Form>
+        <Row>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="exampleEmail">
+                Email
+              </Label>
+              <Input
+                id="exampleEmail"
+                name="email"
+                placeholder="with a placeholder"
+                type="email"
+              />
+            </FormGroup>
+          </Col>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="examplePassword">
+                Password
+              </Label>
+              <Input
+                id="examplePassword"
+                name="password"
+                placeholder="password placeholder"
+                type="password"
+              />
+            </FormGroup>
+          </Col>
+        </Row>
+        <FormGroup>
+          <Label for="exampleAddress">
+            Address
+          </Label>
+          <Input
+            id="exampleAddress"
+            name="address"
+            placeholder="1234 Main St"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleAddress2">
+            Address 2
+          </Label>
+          <Input
+            id="exampleAddress2"
+            name="address2"
+            placeholder="Apartment, studio, or floor"
+          />
+        </FormGroup>
+        <Row>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="exampleCity">
+                City
+              </Label>
+              <Input
+                id="exampleCity"
+                name="city"
+              />
+            </FormGroup>
+          </Col>
+          <Col md={4}>
+            <FormGroup>
+              <Label for="exampleState">
+                State
+              </Label>
+              <Input
+                id="exampleState"
+                name="state"
+              />
+            </FormGroup>
+          </Col>
+          <Col md={2}>
+            <FormGroup>
+              <Label for="exampleZip">
+                Zip
+              </Label>
+              <Input
+                id="exampleZip"
+                name="zip"
+              />
+            </FormGroup>
+          </Col>
+        </Row>
+        <FormGroup check>
+          <Input
+            id="exampleCheck"
+            name="check"
+            type="checkbox"
+          />
+          <Label
+            check
+            for="exampleCheck"
+          >
+            Check me out
+          </Label>
+        </FormGroup>
+        <Button>
+          Sign in
+        </Button>
+      </Form>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    return ( 
+        <div className="header">
+            <h1>
+                Estate care
+            </h1>
+            <h2>
+                Time Sheet
+            </h2>
+        </div>
+     );
+}
+ 
+export default Header;
